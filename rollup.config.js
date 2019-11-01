05499aa63060c5b8cb937636e43c7ca5f022d7c0
2ue <jie746635835@163.com>
2019-11-01 13:58:24 +0800

    增加rollup打包

---
+++ rollup.config.js
@@ -0,0 +1,25 @@
+import resolve from 'rollup-plugin-node-resolve';
+import babel from 'rollup-plugin-babel';
+// import { eslint } from 'rollup-plugin-eslint';
+import commonjs from 'rollup-plugin-commonjs';
+import { uglify } from 'rollup-plugin-uglify';
+export default {
+    input: 'src/cn-nm.js',
+    // sourceMap: 'inline',
+    output: {
+        file: 'dist/index.js',
+        format: 'cjs'
+    },
+    plugins: [
+        resolve(),
+        commonjs(),
+        babel({
+            exclude: 'node_modules/**', // 只编译我们的源代码,
+            runtimeHelpers: true,
+        }),
+        // eslint({
+        //     exclude: 'node_modules/**'
+        // }),
+        uglify()
+    ]
+};
